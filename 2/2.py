8813ec219b991d11f2c2b9e310eb8a6a407f7053
Filip Ghorbani <filip.ghorbani@consid.se>
2022-12-07 08:53:21 +0100

    2_2

---
+++ 2/2.py
@@ -0,0 +1,18 @@
+input = open("2/1.txt", "r")
+
+score = 0
+x = [2, 0, 1, 2, 0]
+for line in input.readlines():
+
+    roundscore = (ord(line[2]) - 88) * 3
+
+    if roundscore == 3:
+        roundscore += ord(line[0])-64
+    elif(roundscore == 0):
+        roundscore += x[(ord(line[0])-64)-1]+1
+    elif(roundscore == 6):
+        roundscore += x[(ord(line[0])-64)+1]+1
+    score += roundscore
+
+
+print(score)
